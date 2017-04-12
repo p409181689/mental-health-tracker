ac272f4e60a52ad65a85cdb4be012293230bd1da
Rose Peruski <rose.peruski@detroitlabs.com>
2017-04-12 14:41:02 -0400

    adding moving to a new page from login

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
     $("#login").click(function(){
         var values = [];
-		$("input[name='text']").each(function() {
+		$("input").each(function() {
 		    values.push($(this).val());
 		});
 		var userName = "Maryla";
  		var password= "Rodowicz1";
 		if (values[0]==userName && values[1]==password) {
-			html("<a href=timestamp.html");
+			window.location.href = "timestamp.html";
 		}
 		else {
 			alert("Try again");
@@ -18,28 +18,6 @@ $(document).ready(function(){
 });
 
 
-//var article_id = $(this).closest('form').find('input[name="article_id"]').val();
-
-// $("button").on("click", function() {
-// 	var userInputName=$("#username").val();
-// 	var userInputPswd=$("#password").val();
-// 	console.log(userInputName);
-// 	console.log(userInputPswd);
-// 	var userName = "Maryla";
-// 		var password= "Rodowicz1";
-
-// 	if(userInputName==userName && userInputPswd== password) {
-// 		console.log("ok!")
-// 	} else {
-// 		console.log("oops!")
-// 	}
-
-// })
 
 
-// $( "#username" ).on( "blur", function() {
-//   $( this ).val(function( i, val ) {
-//     console.log(val);
-//   });
-//   console.log(val);
-// });
+ 
