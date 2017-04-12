4d00341751e433d2ed8c5d2df7ece9ffdb94d706
Rose Peruski <rose.peruski@detroitlabs.com>
2017-04-12 11:52:49 -0400

    starting login logic

---
+++ script.js
@@ -0,0 +1,17 @@
+ $(document).ready(function(){
+ 	var userInputName= $("#userName");
+ 	var userInputPswd= $("")
+ 	var userName = "Maryla";
+	var password= "Rodowicz1";
+
+if(userInputName==userName && userInputPswd== password) {
+	console.log("ok!")
+} else {
+	console.log("oops!")
+}
+ }) 
+
+
+
+
+
