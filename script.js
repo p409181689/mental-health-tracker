a87f5d232ac1a616ca6dfaf30223a8419ad6a399
Rose Peruski <rose.peruski@detroitlabs.com>
2017-04-13 14:33:07 -0400

    adding oject for trigger events

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+// var Event = function(date, time, emotions, location, environment, company, internal, external, notes) {
+// 	this.date=date;
+// 	this.time=time;
+// 	this.emotions=emotions;
+// 	this.location=location;
+// 	this.environment=environment;
+// 	this.company=company;
+// 	this.internal = internal;
+// 	this.external=external;
+// 	this.notes=notes;
+// }
+var nowTime;
+var nowDate;
+var nowEmotions=[];
+// var nowLoc;
+// var nowEnv;
+// var nowComp;
+// var nowInternal;
+// var nowExternal;
+// var nowNotes;
+var eventArray=[];
+
+var Event = function(location, environment, company, internal, external, notes) {
+	this.time=time;
+	this.location=location;
+	this.environment=environment;
+	this.company=company;
+	this.internal = internal;
+	this.external=external;
+	this.notes=notes;
+}
 $(document).ready(function(){
     $("#login").click(function(){
         var values = [];
@@ -28,12 +59,12 @@ $(document).ready(function() {
 	        var year = now.getUTCFullYear();
 
 	       var value= $('#date').val(month + "/"  + day + "/"+ year);
-	       return value;
+	       nowDate=value;
 
 	 });
 	 $(".timestamp input").click(function() {
 		var value = $( "#date" ).datepicker();
-		//return value;
+		nowDate=value;
 		
 
 	});
@@ -53,7 +84,7 @@ $(document).ready(function() {
 	
 	$('select').click(function() {
 		var time = $('option:selected').text();
-		return time;
+		nowTime= time;
 		
 	});
 
@@ -77,24 +108,43 @@ $(document).ready(function() {
 		    emotionArray.push($(this).val());
 		});
 		alert(emotionArray); 
-		return emotionArray;
+		nowEmotions= emotionArray;
     }); 
 
-    
+    $('#trigger-submit').click(function() {
+    	var loc= $('input[name=location]').val();
+    	//nowLoc=loc;
+    	//var loc=$(this).val();
+    	//alert(loc);
+    	var env=$('option:selected').val();//this could be a problem!
+    	//alert(env);
+    	//nowEnv=env;
+    	var comp = $('input[name=company]').val();
+    	nowComp=comp;
+    	//alert(comp);
+    	var inter=$('input[name=internal]').val();
+    	//nowInternal=inter;
+    	//alert(inter);
+    	var exter=$('input[name=external]').val();
+    	//nowExternal=exter;
+    	//alert(exter);
+    	var noted = $('input[name=notes]').val();
+    	//nowNotes=noted;
+    	//alert(noted);
+    	
+    	var event1= new Event(nowDate, nowTime,nowEmotions, loc, env, comp, inter, exter, noted);
+    	eventArray.push(event1);
+    	
+    	//console.log(eventArray);
+    	
+    	//var event1= new Event(value, time, emotionArray, loc, env, comp, inter, exter, noted);
+
+    })
 
 });
 
-var Event = function(date, time, emotions, location, environment, company, internal, external, notes) {
-	this.date=date;
-	this.time=time;
-	this.emotions=emotions;
-	this.location=location;
-	this.environment=environment;
-	this.company=company;
-	this.internal = internal;
-	this.external=external;
-	this.notes=notes;
-}
 
-var event1= new Event(value, time, emotionArray)
+
+
+
  
